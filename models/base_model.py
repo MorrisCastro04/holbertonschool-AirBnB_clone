bbe8790d2e14c4e23d38f3bbae623c31c1f528b4
MorrisCastro <jomarcastro2004@gmail.com>
2024-02-24 11:43:08 -0400

    Add BaseModel class

---
+++ models/base_model.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Base Model Class"""
+import uuid
+from datetime import datetime
+
+
+class BaseModel():
+    """
+    Attributes:
+        id (str): The unique identifier of the instance.
+        created_at (datetime): The datetime when the instance was created.
+        updated_at (datetime): The datetime when the instance was last updated.
+    """
+
+    def __init__(self, id=None, created_at=None, updated_at=None):
+        self.updated_at = datetime.now()
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+
+    def __str__(self):
+        """
+        Returns a string representation of BaseModel.
+        """
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
+    def save(self):
+        """
+        Updates the 'updated_at' attribute with the current datetime.
+        """
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+            """
+            Converts the object to a dictionary representation.
+            """
+            new_dict = self.__dict__
+            new_dict['__class__'] = self.__class__.__name__
+            new_dict['created_at'] = self.created_at.isoformat()
+            new_dict['updated_at'] = self.updated_at.isoformat()
+            return new_dict
