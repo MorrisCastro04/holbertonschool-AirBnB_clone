7d93db5d75e1faa4af8d3de573c96710f9f8721a
MorrisCastro <jomarcastro2004@gmail.com>
2024-02-25 13:59:40 -0400

    rm the init method

--- models/base_model.py
+++ models/base_model.py
@@ -11,12 +11,9 @@ class BaseModel():
         created_at (datetime): The datetime when the instance was created.
         updated_at (datetime): The datetime when the instance was last updated.
     """
-
-    def __init__(self, id=None, created_at=None, updated_at=None):
-        """initializacion of the model class"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+    id = str(uuid.uuid4())
+    created_at = datetime.now()
+    updated_at = datetime.now()
 
     def __str__(self):
         """Returns a string representation of BaseModel."""
