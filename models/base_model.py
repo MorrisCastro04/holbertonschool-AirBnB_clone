4e1d50af914fabd6edea9d6856e02ecd7d9456a7
MorrisCastro <jomarcastro2004@gmail.com>
2024-02-25 13:42:35 -0400

    add dict in line 31

--- models/base_model.py
+++ models/base_model.py
@@ -28,7 +28,7 @@ class BaseModel():
 
     def to_dict(self):
         """Converts the object to a dictionary representation."""
-        new_dict = self.__dict__.copy()
+        new_dict = dict(self.__dict__)
         new_dict['__class__'] = self.__class__.__name__
         new_dict['created_at'] = self.created_at.isoformat()
         new_dict['updated_at'] = self.updated_at.isoformat()
