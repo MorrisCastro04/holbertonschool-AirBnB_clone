04c61e27c1d8016cf709ab237f54d11dabf13d34
MorrisCastro <jomarcastro2004@gmail.com>
2024-02-25 11:58:12 -0400

    add the unittest of the baseModel

--- models/base_model.py
+++ models/base_model.py
@@ -13,13 +13,12 @@ class BaseModel():
     """
 
     def __init__(self, id=None, created_at=None, updated_at=None):
-        self.updated_at = datetime.now()
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
-
+        self.updated_at = self.created_at
     def __str__(self):
         """Returns a string representation of BaseModel."""
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
         """Updates the 'updated_at' attribute with the current datetime."""
---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""unittest for the class BaseModel"""
+import unittest
+from models.base_model import BaseModel
+import uuid
+from datetime import datetime
+
+
+class TestModel(unittest.TestCase):
+    """This class contains unit tests for the BaseModel class."""
+
+    def test_Class(self):
+        """Test the initialization of the BaseModel class."""
+        model = BaseModel()
+        self.assertTrue(hasattr(model, "id"))
+        self.assertTrue(hasattr(model, "created_at"))
+        self.assertTrue(hasattr(model, "updated_at"))
+        self.assertEqual(model.created_at, model.updated_at)
+
+    def test_diferent_id(self):
+        """Test that different instances of BaseModel have different ids."""
+        model_1 = BaseModel()
+        model_2 = BaseModel()
+        self.assertNotEqual(model_1.id, model_2.id)
+
+    def test_str(self):
+        """Test the __str__ method of BaseModel."""
+        model = BaseModel()
+        expected = f"[{model.__class__.__name__}] ({model.id}) {model.__dict__}"
+        self.assertEqual(model.__str__(), expected)
+
+    def test_double_check_id_not_save(self):
+        """Test that the updated not change if is call more than twice."""
+        model = BaseModel()
+        time_1 = model.updated_at
+        time_2 = model.updated_at
+        self.assertEqual(time_1, time_2)
+
+    def test_save(self):
+        """Test the save method of BaseModel."""
+        model = BaseModel()
+        time_1 = model.updated_at
+        model.my_number = 89
+        model.save()
+        time_2 = model.updated_at
+        self.assertNotEqual(time_1, time_2)
+
+    def test_to_dict(self):
+        """Test the to_dict method of BaseModel."""
+        model = BaseModel()
+        model_dict = model.to_dict()
+        self.assertTrue(isinstance(model_dict, dict))
+        self.assertTrue(isinstance(model, BaseModel))
