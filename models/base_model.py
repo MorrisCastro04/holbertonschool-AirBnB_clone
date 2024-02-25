f5eebeaf803b15f2435585b916c6b29355c51d90
MorrisCastro <jomarcastro2004@gmail.com>
2024-02-25 14:12:41 -0400

    change the updated_at

--- models/base_model.py
+++ models/base_model.py
@@ -16,7 +16,7 @@ class BaseModel():
         """initializacion of the model class"""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        self.updated_at = datetime.now()
 
     def __str__(self):
         """Returns a string representation of BaseModel."""
