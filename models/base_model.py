77686e02a9a99a24ceb513e071a300b881ac1500
MorrisCastro <jomarcastro2004@gmail.com>
2024-02-25 12:03:27 -0400

    fix pycodestyle

--- models/base_model.py
+++ models/base_model.py
@@ -16,6 +16,7 @@ class BaseModel():
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = self.created_at
+
     def __str__(self):
         """Returns a string representation of BaseModel."""
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
