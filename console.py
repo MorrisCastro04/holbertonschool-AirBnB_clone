059b9c22c92f201bd13468d633438467851659e8
MorrisCastro <jomarcastro2004@gmail.com>
2024-02-28 18:27:25 -0400

    add all the other classes to the CMD class

--- console.py
+++ console.py
@@ -1,12 +1,42 @@
 import cmd
 from models.base_model import BaseModel
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.user import User
+from models.review import Review
+from models.state import State
 from models import storage
+
 
 class HBNBCommand(cmd.Cmd):
     """Command interpreter for HBNB project."""
     
     prompt = "(hbnb) "
-    
+    class_dict = {
+        "BaseModel": BaseModel,
+        "Amenity": Amenity,
+        "City": City,
+        "Place": Place,
+        "User": User,
+        "Review": Review,
+        "State": State
+    }
+
+
+    def emptyline(self):
+        """Called when an empty line is entered in response to the prompt."""
+        pass
+
+    def do_EOF(self, arg):
+        """EOF command to exit the program"""
+        print("")
+        return True
+
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
+        return True
+
     def do_create(self, arg):
         """Creates a new instance of BaseModel, saves it, and prints the id."""
         if not arg:
@@ -81,50 +111,35 @@ class HBNBCommand(cmd.Cmd):
             print([str(obj) for key, obj in all_objs.items() if key.startswith(args[0])])
 
     def do_update(self, arg):
-        """Updates an instance based on the class name and id."""
+        """
+        Updates an instance based on the class name and id by adding or
+        updating attribute (value should be without double quotes).
+        """
         args = arg.split()
-        if not args:
+        if len(args) < 1:
             print("** class name missing **")
             return
-        try:
-            class_name = args[0]
-            if class_name not in globals():
-                print("** class doesn't exist **")
-                return
-            if len(args) < 2:
-                print("** instance id missing **")
-                return
-            instance_id = args[1]
-            key = "{}.{}".format(class_name, instance_id)
-            all_objs = storage.all()
-            if key not in all_objs:
-                print("** no instance found **")
-                return
-            if len(args) < 3:
-                print("** attribute name missing **")
-                return
-            if len(args) < 4:
-                print("** value missing **")
-                return
-            attr_name = args[2]
-            attr_value = args[3]
-            setattr(all_objs[key], attr_name, attr_value)
-            storage.save()
-        except Exception as e:
-            print(e)
-
-    def emptyline(self):
-        """Called when an empty line is entered in response to the prompt."""
-        pass
-
-    def do_EOF(self, arg):
-        """EOF command to exit the program"""
-        print("")
-        return True
-
-    def do_quit(self, arg):
-        """Quit command to exit the program"""
-        return True
+        if args[0] not in self.class_dict:
+            print("** class doesn't exist **")
+            return
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+        if len(args) < 3:
+            print("** attribute name missing **")
+            return
+        if len(args) < 4:
+            print("** value missing **")
+            return
+        key = f"{args[0]}.{args[1]}"
+        all_objs = storage.all()
+        if key not in all_objs:
+            print("** no instance found **")
+            return
+        obj = all_objs[key]
+        value = args[3].strip("\"")
+        setattr(obj, args[2], value)
+        obj.save()
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
